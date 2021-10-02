63960148727d53f5bc2d65c68946b62ffaa017dd
tutor-mani <91711519+tutor-mani@users.noreply.github.com>
2021-10-02 09:58:18 +0800

    Revert "Initial Check-in"
    This reverts commit 8701bed58ebbe5b9ff7cc1befe95cfbeebdf1616.

--- config.js
+++
@@ -1,9 +0,0 @@
-const envConfig = {
-    APP_PORT: 8080,
-    S3_BUCKET_NAME: "s3-sample-reader",
-    S3_ACCESS_KEY: "AKIAZOWNHOT4QDWIKF5E",
-    S3_SECRET_KEY: "e9HgIY33DAm8D3jxGlHioNaJa88rAGz6vfX1QMVq",
-    S3_REGION: "ap-southeast-1"
-}
-
-module.exports = envConfig
